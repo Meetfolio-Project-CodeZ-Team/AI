56e82606a0195c493bafb90c6a753f1ed9dcf4d6
kylo-deb <rlagusrua3687@gmail.com>
2024-05-07 16:23:34 +0900

    :zap: feat: 역량 키워드, 지원 직무 Request Body 예외 처리

--- crud/gpt_crud.py
+++ crud/gpt_crud.py
@@ -2,10 +2,27 @@ from sqlalchemy.orm import Session
 from domains.coverletter import Coverletter
 from datetime import datetime
 from domains.feedback import Feedback
+from sqlalchemy.orm.exc import NoResultFound
+from flask import abort
 
 def get_coverletter(db: Session, coverletter_id: int):
 
-  return db.query(Coverletter).filter(Coverletter.cover_letter_id == coverletter_id).one()
+  # 예외 처리
+  try:
+    cover_letter = db.query(Coverletter).filter(Coverletter.cover_letter_id == coverletter_id).one()
+
+    if not cover_letter.job_keyword:
+      abort(400, description = "Job Keyword is missing!")
+    if len(cover_letter.answer) < 200:
+      abort(400, description = "자기소개서 글자수가 너무 적습니다.")
+    if not cover_letter.keyword_1 or not cover_letter.keyword_2:
+      abort(400, description = "Keyword1 or Keyword2 is missing!!")
+    
+    return cover_letter
+  except NoResultFound:
+    abort(400, description="Cover letter with ID {} not found.".format(coverletter_id))
+
+
 
 def save_feedback(db: Session, cover_letter_id, response):
   feedback = Feedback(
@@ -20,4 +37,4 @@ def save_feedback(db: Session, cover_letter_id, response):
   db.add(feedback)
   db.commit()
 
-  return 1
+  return feedback.solution_id
--- crud/kobert_crud.py
+++ crud/kobert_crud.py
@@ -7,17 +7,22 @@ from json import dumps
 from core.config import settings
 from datetime import datetime
 from sqlalchemy.orm.exc import NoResultFound
+from flask import abort
 
 def patch_coverletter(db: Session, cover_letter_id, data):
 
-  cover_letter = db.query(Coverletter).filter(Coverletter.cover_letter_id==cover_letter_id).first()
-  if cover_letter:
-    cover_letter.keyword_1 = data['keyword1']
-    cover_letter.keyword_2 = data['keyword2']
-    cover_letter.job_keyword = data['job_keyword']
-    db.commit()
-  else:
-    return "Coverletter Not Found"
+  # 예외 처리
+  try:
+    cover_letter = db.query(Coverletter).filter(Coverletter.cover_letter_id==cover_letter_id).first()
+    if not data['keyword1'] or not data['keyword2'] or not data['job_keyword']:
+      abort(400, description = '직무 키워드와 역량 키워드 1, 2를 작성해주세요!!')
+    if cover_letter:
+      cover_letter.keyword_1 = data['keyword1']
+      cover_letter.keyword_2 = data['keyword2']
+      cover_letter.job_keyword = data['job_keyword']
+      db.commit()
+  except NoResultFound:
+    abort(400, description="Cover letter with ID {} not found.".format(coverletter_id))
 
 def save_analysis(db: Session, cover_letter_id, job_suitability, skill_keywords):
   analysis = Analysis(
@@ -44,9 +49,6 @@ def get_inactive_dataset(db: Session) -> str:
       }
       dataset_list.append(dataset_dict)
   
-  # 가져온 데이터셋 -> 'ACTIVE'로 벼경
-  # for dataset in datasets:
-  #   dataset.status = 'ACTIVE'
   return datasets, dataset_list
 
 def get_active_model(db: Session):
@@ -55,7 +57,7 @@ def get_active_model(db: Session):
     model_path = model.file_path
 
   except NoResultFound:
-    model_path = '/home/t24105/v0.9src/ai/model/meetfolio_model_v1.pt'
+    model_path = settings.KOBERT_DEFAULT
 
   return model_path
 
